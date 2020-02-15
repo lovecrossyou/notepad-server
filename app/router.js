3398c66ece72bf599dccacc8503821e41897a1e6
lovecrossyou <lovecrossyoutt@hotmail.com>
2020-02-15 09:55:09 +0800

    save all

--- app/router.js
+++ app/router.js
@@ -6,9 +6,5 @@
 module.exports = app => {
   const { router, controller, io } = app;
   router.get('/', controller.home.index);
-  // io.of('/').route('chat', io.controller.chat.index);
   app.io.of('/').route('chat', app.io.controller.chat);
-  // app.io.of('/').route('chat', app.io.controller.nps.exchange);
-  // app.io.route('chat', app.io.controller.chat.ping);
-
 };
